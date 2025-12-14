eb54d3f05d97d79fd87f98af7dab35b634cf4dda
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2025-12-14 21:41:55 +0530

    Refactor axiosInstance configuration for backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
-export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api",
-    withCredentials:true
-}
+import axios from "axios";
 
-)
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : import.meta.env.VITE_BACKEND_URL + "/api",
+  withCredentials: true,
+});
