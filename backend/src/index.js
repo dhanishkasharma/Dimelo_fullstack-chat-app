773e06dacec5cb06f664232ceb09f4b55552090b
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2025-12-15 12:33:51 +0530

    Add allowed methods to CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,8 @@ app.use(cookieParser());
 app.use(cors(
     {
          origin: process.env.CLIENT_URL,
-        credentials: true
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
     }
 ));
 
